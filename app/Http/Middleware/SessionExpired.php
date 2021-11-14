674d7e92aa04c61222bd5a133f511286191b0a96
AKS144 <abhi144k@gmail.com>
2021-11-14 13:27:58 +0530

    first commit

---
+++ app/Http/Middleware/SessionExpired.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Auth;
+use Session;
+use Illuminate\Session\Store;
+
+class SessionExpired
+{
+    protected $session;
+    protected $timeout = 120;
+    
+    public function __construct(Store $session){
+        $this->session = $session;
+    }
+  
+    public function handle(Request $request, Closure $next)
+    {
+        $isLoggedIn = $request->path() != 'dashboard/logout';
+        if(! session('lastActivityTime'))
+            $this->session->put('lastActivityTime', time());
+        elseif(time() - $this->session->get('lastActivityTime') > $this->timeout){
+            $this->session->forget('lastActivityTime');
+            $cookie = cookie('intend', $isLoggedIn ? url()->current() : 'dashboard');
+            auth()->logout();
+        }
+        $isLoggedIn ? $this->session->put('lastActivityTime', time()) : $this->session->forget('lastActivityTime');
+        return $next($request);
+    }
+}
